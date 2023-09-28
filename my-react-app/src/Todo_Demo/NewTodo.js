492ff068895b1a781b3aa0519c9255fe8814bd0a
rabbi-hosen <rabbihosen01581@gmail.com>
2023-09-28 23:37:18 +0600

    basic-todo-app

---
+++ my-react-app/src/Todo_Demo/NewTodo.js
@@ -0,0 +1,30 @@
+
+import React, { useState } from 'react'
+
+const NewTodo = (props) => {
+
+    const [todo,setTodo] = useState("");
+
+    const handleInputChange = (event) => {
+        setTodo(event.target.value)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.onTodo(todo)
+    }
+   
+  return (
+     <form onSubmit={handleSubmit}>
+        <label htmlFor='todo'>New Todo: </label>
+        <input 
+        type="text" 
+        name="todo"  
+        id="todo" 
+        value={todo} 
+        onChange={handleInputChange}/>
+        <button type='submit'>Add Todo</button>
+     </form>
+  )
+}
+
+export default NewTodo
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'
+import ReactDOM from 'react-dom';
 
 
+import App from "./Todo_Demo/App"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    
   <App />
 
 
-);
+)
